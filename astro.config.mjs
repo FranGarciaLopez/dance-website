7305a0977d17a1f51f61af5f12317427ab8112b9
Fran <frangalo34@gmail.com>
2025-05-19 17:15:33 +0200

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap(), solid()],
+  site: "https://thelatinaction.com",
 });
