7eadba890373ce80ebcd03c7293ec0886acfb866
Fran <frangalo34@gmail.com>
2025-08-10 13:27:18 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,23 @@ import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), solid()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => {
+        // Exclude specific pages from the sitemap http
+        /* if (page.includes("/404") || page.includes("/500")) {
+          return false;
+        } */
+        // Include all other pages in the sitemap
+
+        return true;
+      },
+      changefreq: "weekly",
+      priority: 0.5,
+      trailingSlash: "never",
+    }),
+    solid(),
+  ],
   site: "https://thelatinaction.com",
 });
