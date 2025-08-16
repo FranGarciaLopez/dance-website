4d7aab8aa42e3700e8e9742c567bb92465a16320
Fran <frangalo34@gmail.com>
2025-08-16 17:50:29 +0200

    add new images

---
+++ src/data/galleryImages.ts
@@ -0,0 +1,9 @@
+export const galleryImages = [
+  "foto_alumnado_intensivo_bachata_lady_style_dairen_-_Luna_the_latin_action_hgoqus",
+  "Foto_Luna_The_Latin_action_Salsa_y_Bachata_-_juancarlosyluna.lc_-_aiinhoa.garciia_y_cann.pd_q2zvg6",
+  "foto_alumnado_taller_de_bachata_bubaloo_-_The_Latin_Action_Salsa_y_Bachata_-_Juan_Carlos_y_Luna_wwbezy",
+  "Foto_Juan_Carlos_The_Latin_action_Salsa_y_Bachata_-_juancarlosyluna.lc_-_aiinhoa.garciia_y_cann.pd_ufca6d",
+  "Juan_Carlos_-_The_Latin_Action_Salsa_y_Bachata_-_Lions_Alicante_htaypt",
+  "Luna_The_Latin_Action_Salsa_y_Bachata_-_Bachata_Social_ob9n2c",
+  "Foto_taller_de_Bachata_Juan_Carlos_y_Luna_The_Latin_Action_Salsa_y_Bachata_-_Lyons_Alicante_2025_i6fspa",
+];
